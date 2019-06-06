95e1ca9d120f281979a2f3b2cc3f7e13de586cbe
Keita Kagurazaka <kei.simple@gmail.com>
2019-06-07 08:05:57 +0900

    Remove unused code

--- processor/src/main/java/com/kkagurazaka/reactive/repository/processor/writer/RepositoryWriter.kt
+++ processor/src/main/java/com/kkagurazaka/reactive/repository/processor/writer/RepositoryWriter.kt
@@ -3,10 +3,6 @@ package com.kkagurazaka.reactive.repository.processor.writer
 import com.kkagurazaka.reactive.repository.processor.ProcessingContext
 import com.kkagurazaka.reactive.repository.processor.definition.EntityDefinition
 import com.kkagurazaka.reactive.repository.processor.definition.RepositoryDefinition
-import com.kkagurazaka.reactive.repository.processor.tools.Types
-import com.squareup.javapoet.ClassName
-import com.squareup.javapoet.FieldSpec
-import com.squareup.javapoet.ParameterizedTypeName
 import com.squareup.javapoet.TypeSpec
 import javax.lang.model.element.Modifier
 
@@ -30,30 +26,4 @@ abstract class RepositoryWriter<RD : RepositoryDefinition<out Annotation, out En
     protected abstract fun verify()
 
     protected abstract fun TypeSpec.Builder.setup(): TypeSpec.Builder
-
-    private fun buildProcessorFieldSpec(entityClassName: ClassName, createDefault: Boolean): FieldSpec =
-        FieldSpec
-            .builder(
-                ParameterizedTypeName.get(Types.rx2BehaviorProcessor, entityClassName),
-                "processor",
-                Modifier.PRIVATE
-            )
-            .apply {
-                if (createDefault) {
-                    initializer("BehaviorProcessor.createDefault(new \$T())", entityClassName)
-                } else {
-                    initializer("BehaviorProcessor.create()")
-                }
-            }
-            .build()
-
-    private fun buildSerializedFieldSpec(entityClassName: ClassName, processor: FieldSpec): FieldSpec =
-        FieldSpec
-            .builder(
-                ParameterizedTypeName.get(Types.rx2FlowableProcessor, entityClassName),
-                "serialized",
-                Modifier.PRIVATE
-            )
-            .initializer("\$N.toSerialized()", processor)
-            .build()
 }

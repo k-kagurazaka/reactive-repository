22c2de803c9da89c6d32ceef80fb9dbb19422c7b
Keita Kagurazaka <kei.simple@gmail.com>
2019-06-11 11:39:31 +0900

    Add tests

--- sample/src/main/java/com/kkagurazaka/reactive/repository/sample/KotlinDataClassEntity.kt
+++ sample/src/main/java/com/kkagurazaka/reactive/repository/sample/KotlinDataClassEntity.kt
@@ -8,10 +8,10 @@ import com.kkagurazaka.reactive.repository.annotation.PrefsKey
 @PrefsEntity(typeAdapter = SomeClassListTypeAdapter::class)
 data class KotlinDataClassEntity(
     @get:PrefsKey val isVeteran: Boolean = false,
-    @get:PrefsKey val someStr: String = "",
+    @get:PrefsKey val someStr: String? = null,
     @get:PrefsKey val age: Int = -1,
     @get:PrefsKey val pie: Float = 3.1415f,
     @get:PrefsKey val amount: Long = 123456789L,
-    @get:PrefsKey val strList: Set<String> = setOf("1", "2", "3"),
-    @get:PrefsKey val someClassList: List<SomeClass> = emptyList()
+    @get:PrefsKey val strList: Set<String> = emptySet(),
+    @get:PrefsKey val someClassList: List<SomeClass> = listOf(SomeClass("initial"))
 )
--- sample/src/main/java/com/kkagurazaka/reactive/repository/sample/TypeAdapterInstanceRequired.kt
+++ sample/src/main/java/com/kkagurazaka/reactive/repository/sample/TypeAdapterInstanceRequired.kt
@@ -1,12 +1,19 @@
 package com.kkagurazaka.reactive.repository.sample
 
-import android.content.Context
 import com.kkagurazaka.reactive.repository.annotation.PrefsEntity
 import com.kkagurazaka.reactive.repository.annotation.PrefsKey
 import com.kkagurazaka.reactive.repository.annotation.PrefsRepository
+import io.reactivex.Flowable
+import io.reactivex.Observable
 
 @PrefsEntity(typeAdapter = SomeClassListInstanceRequiredTypeAdapter::class)
 data class TypeAdapterInstanceRequiredEntity(
+    @get:PrefsKey val isVeteran: Boolean = false,
+    @get:PrefsKey val someStr: String? = null,
+    @get:PrefsKey val age: Int = -1,
+    @get:PrefsKey val pie: Float = 3.1415f,
+    @get:PrefsKey val amount: Long = 123456789L,
+    @get:PrefsKey val strList: Set<String> = emptySet(),
     @get:PrefsKey val someClassList: List<SomeClass> = listOf(SomeClass("initial"))
 )
 
@@ -15,8 +22,9 @@ interface TypeAdapterInstanceRequiredRepository {
 
     fun get(): TypeAdapterInstanceRequiredEntity
 
-    fun save(entity: TypeAdapterInstanceRequiredEntity?)
-}
+    fun observe(): Observable<TypeAdapterInstanceRequiredEntity>
 
-fun createTypeAdapterInstanceRequiredRepository(context: Context): TypeAdapterInstanceRequiredRepository =
-    TypeAdapterInstanceRequiredRepositoryImpl(context, SomeClassListInstanceRequiredTypeAdapter())
+    fun observeWithBackpressure(): Flowable<TypeAdapterInstanceRequiredEntity>
+
+    fun store(entity: TypeAdapterInstanceRequiredEntity?)
+}
